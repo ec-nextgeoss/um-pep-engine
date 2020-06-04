64406c0bf88474481f4d9a08c91ed78ea571a4d2
mamuniz <mamuniz@elecnor.es>
2020-06-04 13:22:14 +0200

    EOEPCA-110 #Kubernetes files and env var

--- src/main.py
+++ src/main.py
@@ -11,9 +11,41 @@ from config import load_config, save_config
 from eoepca_scim import EOEPCA_Scim, ENDPOINT_AUTH_CLIENT_POST
 from custom_oidc import OIDCHandler
 from custom_uma import UMA_Handler
+import os
 ### INITIAL SETUP
+
+env_vars = [
+"PEP_REALM",
+"PEP_AUTH_SERVER_URL",
+"PEP_PROXY_ENDPOINT",
+"PEP_SERVICE_HOST",
+"PEP_SERVICE_PORT",
+"PEP_S_MARGIN_RPT_VALID",
+"PEP_CHECK_SSL_CERTS",
+"PEP_USE_THREADS",
+"PEP_DEBUG_MODE",
+"PEP_RESOURCE_SERVER_ENDPOINT"]
+
+use_env_var = True
+
+for env_var in env_vars:
+    if env_var not in os.environ:
+        use_env_var = False
+
+g_config = {}
 # Global config objects
-g_config = load_config("config/config.json")
+if use_env_var is False:
+    g_config = load_config("config/config.json")
+else:
+    for env_var in env_vars:
+        env_var_config = env_var.replace('PEP_', '')
+
+        if "true" in os.environ[env_var].replace('"', ''):
+            g_config[env_var_config.lower()] = True
+        elif "false" in os.environ[env_var].replace('"', ''):
+            g_config[env_var_config.lower()] = False
+        else:
+            g_config[env_var_config.lower()] = os.environ[env_var].replace('"', '')
 
 # Global handlers
 g_wkh = WellKnownHandler(g_config["auth_server_url"], secure=False)
@@ -29,11 +61,15 @@ if "client_id" not in g_config or "client_secret" not in g_config:
                                 responseTypes = ["code","token","id_token"],
                                 scopes = ['openid', 'uma_protection', 'permission'],
                                 token_endpoint_auth_method = ENDPOINT_AUTH_CLIENT_POST)
-    print("NEW CLIENT created with ID '"+new_client["client_id"]+"', since no client config was found on config.json")
+    print("NEW CLIENT created with ID '"+new_client["client_id"]+"', since no client config was found on config.json or environment")
 
     g_config["client_id"] = new_client["client_id"]
     g_config["client_secret"] = new_client["client_secret"]
-    save_config("config/config.json", g_config)
+    if use_env_var is False:
+        save_config("config/config.json", g_config)
+    else:
+        os.environ["PEP_CLIENT_ID"] = new_client["client_id"]
+        os.environ["PEP_CLIENT_SECRET"] = new_client["client_secret"]
     print("New client saved to config!")
 else:
     print("Client found in config, using: "+g_config["client_id"])
@@ -72,7 +108,7 @@ def resource_request(path):
         print("Token found: "+rpt)
         rpt = rpt.replace("Bearer ","").strip()
         # Validate for a specific resource
-        if uma_handler.validate_rpt(rpt, [{"resource_id": resource_id, "resource_scopes": scopes }], g_config["s_margin_rpt_valid"]):
+        if uma_handler.validate_rpt(rpt, [{"resource_id": resource_id, "resource_scopes": scopes }], int(g_config["s_margin_rpt_valid"])):
             print("RPT valid, accesing ")
             # redirect to resource
             try:
@@ -112,6 +148,6 @@ def resource_request(path):
 app.run(
     debug=g_config["debug_mode"],
     threaded=g_config["use_threads"],
-    port=g_config["service_port"],
+    port=int(g_config["service_port"]),
     host=g_config["service_host"]
 )
